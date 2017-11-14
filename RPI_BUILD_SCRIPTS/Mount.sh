aae2694bffab6bd98e8983acd5fdef47375b0fc4
Ros <liandtow@gmail.com>
2017-11-14 18:52:34 +0000

    mount.sh mod

--- RPI_BUILD_SCRIPTS/Mount.sh
+++ RPI_BUILD_SCRIPTS/Mount.sh
@@ -1,5 +1,4 @@
 sudo mkdir /mnt/ext4 &
 sudo mkdir /mnt/fat32 &
-sudo mount /dev/$RPI_BOOT_PARTITION /mnt/fat32/
-sudo mount /dev/$RPI_ROOT_PARTITION /mnt/ext4/
+sudo mount /dev/$RPI_BOOT_PARTITION /mnt/fat32/ && sudo mount /dev/$RPI_ROOT_PARTITION /mnt/ext4/
 
