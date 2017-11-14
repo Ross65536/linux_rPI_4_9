f5ef75564c1e4250df77bd4ee3653cb5a24cbf1b
Ros <liandtow@gmail.com>
2017-11-14 17:03:34 +0000

    working version

--- RPI_BUILD_SCRIPTS/1_Build_rPi.sh
+++ RPI_BUILD_SCRIPTS/1_Build_rPi.sh
@@ -1,5 +1,5 @@
 cd .. 
 KERNEL=kernel
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j 8 2> cat RPI_BUILD_SCRIPTS/BUILD_ERRORS
-make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs -j 8 2>> cat RPI_BUILD_SCRIPTS/BUILD_ERRORS
-
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- -j 8 2> RPI_BUILD_SCRIPTS/BUILD_ERRORS
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs -j 8 2>> RPI_BUILD_SCRIPTS/BUILD_ERRORS
+cat RPI_BUILD_SCRIPTS/BUILD_ERRORS
